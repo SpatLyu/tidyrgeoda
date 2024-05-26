ca0725d1a58f8b25457c9ac7077bb0d616ba23f7
SpatLyu <lyu.geosocial@gmail.com>
2024-05-26 20:42:30 +0800

    Add support for univariate spatial stratification

--- R/tidyrgeoda_helper.R
+++ R/tidyrgeoda_helper.R
@@ -32,3 +32,58 @@ scale_fill_lisa = \(name = 'LISA',...){
                              c("Not significant","High-High","Low-Low","Low-High",
                                "High-Low","Undefined","Isolated")))
 }
+
+
+#' @title Univariate Spatial Stratification
+#' @author Wenbo Lv
+#' @description
+#' Univariate Spatial Stratification by invoking rgeoda's *_breaks function.
+#'
+#' @param sfj An sf, tibble or data.frame object
+#' @param varcol The variables selected to run univariate spatial stratification.
+#' @param break_method (optional) Which has to be one of "stddev"(default), "hinge15",
+#' "hinge30", "percentile", "natural", "quantile". When the `break_method` is "natural"
+#' or "quantile", you can assign the `k` argument to have how many breaks, other methods
+#' will have 6 groups.
+#' @param k (optional)A numeric value indicates how many breaks,default is 6.
+#'
+#' @return A vector of numeric values of computed breaks
+#' @export
+#'
+#' @examples
+#' library(sf)
+#' guerry = read_sf(system.file("extdata", "Guerry.shp", package = "rgeoda"))
+#' st_breaks(guerry,'Crm_prs',break_method = "quantile", k = 5)
+st_breaks = \(sfj,varcol,break_method = 'stddev',k = 6){
+  if (inherits(sfj,"sf")) {
+    df = sfj %>%
+      sf::st_drop_geometry() %>%
+      dplyr::select(dplyr::all_of(varcol))
+  } else {
+    df = sfj %>%
+      dplyr::select(dplyr::all_of(varcol))
+  }
+
+  switch(break_method,
+         "stddev" = {
+           bv = rgeoda::stddev_breaks(df)
+         },
+         "hinge15" = {
+           bv = rgeoda::hinge15_breaks(df)
+         },
+         "hinge30" = {
+           bv = rgeoda::hinge30_breaks(df)
+         },
+         "percentile" = {
+           bv = rgeoda::percentile_breaks(df)
+         },
+         "natural" = {
+           bv = rgeoda::natural_breaks(k,df)
+         },
+         "quantile" = {
+           bv = rgeoda::quantile_breaks(k,df)
+         }
+  )
+
+  return(bv)
+}
